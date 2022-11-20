a286ca9a4a8fb13c4ca08ee0ffcd91d857eb4092
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2022-11-20 15:26:26 +0100

    fix total count and refactor

--- ActivityTrackerServer/src/main/java/com/agh/activitytrackerserver/repository/CustomUserLogRepositoryImpl.java
+++ ActivityTrackerServer/src/main/java/com/agh/activitytrackerserver/repository/CustomUserLogRepositoryImpl.java
@@ -1,10 +1,11 @@
 package com.agh.activitytrackerserver.repository;
 
+import com.agh.activitytrackerclient.models.ActivityUser;
 import com.agh.activitytrackerclient.models.UserLog;
 
 import com.agh.activitytrackerclient.models.UserWithLogsCount;
 import com.agh.activitytrackerserver.transport.*;
-import org.springframework.data.domain.Page;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -13,14 +14,18 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Repository
 public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
+    private final ActivityUserRepository activityUserRepository;
     private final EntityManager em;
 
-    public CustomUserLogRepositoryImpl(EntityManager em) {
-
+    public CustomUserLogRepositoryImpl(ActivityUserRepository activityUserRepository, EntityManager em) {
+        this.activityUserRepository = activityUserRepository;
         this.em = em;
     }
     @Override
@@ -32,8 +37,9 @@ public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
         CriteriaQuery<String> cq = criteriaBuilder.createQuery(String.class);
 
         Root<UserLog> root = q.from(UserLog.class);
+        Root<UserLog> cRoot = cq.from(UserLog.class);
 
-        cq.select(cq.from(UserLog.class).get("endpoint")).distinct(true);
+        cq.select(cRoot.get("endpoint")).distinct(true);
 
         q.multiselect(root.get("endpoint"), criteriaBuilder.count(root));
 
@@ -52,27 +58,22 @@ public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
                 break;
         };
         ArrayList<Predicate> predicates = new ArrayList<>();
+        ArrayList<Predicate> cPredicates = new ArrayList<>();
 
         var ep = query.getEndpointName();
         if(ep != null && ep.length() > 0) {
             predicates.add(criteriaBuilder.like(root.get("endpoint").as(String.class), "%" + ep + "%"));
+            cPredicates.add(criteriaBuilder.like(cRoot.get("endpoint").as(String.class), "%" + ep + "%"));
         }
         var userId = query.getActivityUserId();
 
         if(userId != null && userId.length() > 0) {
             predicates.add(criteriaBuilder.equal(root.get("activityUserId"), userId));
+            cPredicates.add(criteriaBuilder.equal(cRoot.get("activityUserId"), userId));
         }
 
-        TimeRange timeRange = query.getTimeRange();
-
-        if (timeRange != null) {
-            if (timeRange.getTo() > 0) {
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("activityEnd"), timeRange.getTo()));
-            }
-            if (timeRange.getFrom() > 0) {
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("activityEnd"), timeRange.getFrom()));
-            }
-        }
+        predicates.addAll(getTimeRangePredicates(query.getTimeRange(), root, criteriaBuilder));
+        cPredicates.addAll(getTimeRangePredicates(query.getTimeRange(), cRoot, criteriaBuilder));
 
         if (!predicates.isEmpty()) {
             q.where(predicates.toArray(new Predicate[0]));
@@ -96,9 +97,10 @@ public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
                 .createQuery(UserWithLogsCount.class);
 
         Root<UserLog> root = q.from(UserLog.class);
-
         CriteriaQuery<String> cq = criteriaBuilder.createQuery(String.class);
-        cq.select(cq.from(UserLog.class).get("activityUserId")).distinct(true);
+
+        Root<UserLog> cRoot = cq.from(UserLog.class);
+        cq.select(cRoot.get("activityUserId")).distinct(true);
 
         q.multiselect(root.get("activityUserId"), criteriaBuilder.count(root));
 
@@ -112,11 +114,54 @@ public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
                 q.orderBy(criteriaBuilder.desc(criteriaBuilder.count(root.get("activityUserId"))));
                 break;
         };
-        ArrayList<Predicate> predicates = new ArrayList<Predicate>();
+        ArrayList<Predicate> predicates = new ArrayList<>();
+        ArrayList<Predicate> cPredicates = new ArrayList<>();
 
         predicates.add(criteriaBuilder.isNotNull(root.get("activityUserId")));
+        cPredicates.add(criteriaBuilder.isNotNull(cRoot.get("activityUserId")));
 
-        TimeRange timeRange = query.getTimeRange();
+        predicates.addAll(getTimeRangePredicates(query.getTimeRange(), root, criteriaBuilder));
+        cPredicates.addAll(getTimeRangePredicates(query.getTimeRange(), cRoot, criteriaBuilder));
+
+        if (!predicates.isEmpty()) {
+            q.where(predicates.toArray(new Predicate[0]));
+            cq.where(cPredicates.toArray(new Predicate[0]));
+        }
+
+        List<String> uniqueUserIds = em.createQuery(cq).getResultList();
+        var uniqueUsers = getActivityUsersWhereIdIn(uniqueUserIds);
+
+        // Log don't have to have activity user so join is not an option(at least not in easy way :D)
+        String username = query.getUsername();
+        if (username != null && username.length() > 0) {
+            uniqueUsers = uniqueUsers
+                    .stream()
+                    .filter(x -> x.getUsername().matches(username))
+                    .collect(Collectors.toList());
+        }
+
+        List<UserWithLogsCount> results = em.createQuery(q)
+                .setFirstResult(query.getPage() * query.getPageSize())
+                .setMaxResults(query.getPageSize())
+                .getResultList();
+
+        List<UserWithLogsCount> fiteredByUsernameResults = new LinkedList<>();
+
+        for(var userWithCount: results) {
+            if(uniqueUsers.stream().anyMatch(u -> Objects.equals(u.getId(), userWithCount.getActivityUserId()))){
+                fiteredByUsernameResults.add(userWithCount);
+            }
+        }
+
+        return new PageResponse<>(fiteredByUsernameResults, uniqueUsers.size());
+    }
+
+    private List<ActivityUser> getActivityUsersWhereIdIn(List<String> ids) {
+        return activityUserRepository.findAllById(ids);
+    }
+
+    private <T> List<Predicate> getTimeRangePredicates(TimeRange timeRange, Root<T> root, CriteriaBuilder criteriaBuilder) {
+        ArrayList<Predicate> predicates = new ArrayList<>();
 
         if (timeRange != null) {
             if (timeRange.getTo() > 0) {
@@ -127,18 +172,6 @@ public class CustomUserLogRepositoryImpl implements CustomUserLogRepository {
             }
         }
 
-        if (!predicates.isEmpty()) {
-            q.where(predicates.toArray(new Predicate[0]));
-        }
-
-        List<UserWithLogsCount> results = em.createQuery(q)
-            .setFirstResult(query.getPage() * query.getPageSize())
-            .setMaxResults(query.getPageSize())
-            .getResultList();
-
-        List<String> uniqueUsers = em.createQuery(cq).getResultList();
-
-
-        return new PageResponse<>(results, uniqueUsers.size());
+        return  predicates;
     }
 }
